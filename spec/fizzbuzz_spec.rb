62ee7cb2b68e326eef12fbdf1a8441b8caa0d5a3
Rizwan Ali <rizcube@gmail.com>
2015-04-28 14:56:27 +0100

    it returns fizz, buzz, Fizzbuzz accordingly

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -28,6 +28,22 @@ describe 'FizzBuzz' do
       expect(is_divisible_by_fifteen?(1)).not_to eq true
     end
 
+    context 'while playing the game it returns' do
+      it 'the number' do
+        expect(fizzbuzz(1)).to eq 1
+      end
 
+      it 'Fizz' do
+        expect(fizzbuzz(3)).to eq 'Fizz'
+      end
+
+      it 'Buzz' do
+        expect(fizzbuzz(5)).to eq 'Buzz'
+      end
+
+      it 'FizzBuzz' do
+        expect(fizzbuzz(15)).to eq 'FizzBuzz'
+      end
+    end
   end
 end
