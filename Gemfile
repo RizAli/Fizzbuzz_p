3549408fcb71f8a8211980662e5c7224ca2a219a
Rizwan Ali <rizcube@gmail.com>
2015-04-28 14:30:19 +0100

    it knows if the number is divisible by 3,5,15 or not

--- Gemfile
+++ Gemfile
@@ -6,9 +6,3 @@ group :test do
   gem 'rspec-collection_matchers'
   gem "codeclimate-test-reporter", require: nil
 end
-
-group :development do
-  gem 'guard'
-  gem 'guard-rspec'
-  gem 'pry'
-end
--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,13 @@
-def fizzbuzz(number)
-  number%3 == 0
-  'fizz'
+def is_divisible_by_three?(number)
+  number % 3 == 0
 end
+
+def is_divisible_by_five?(number)
+  number % 5 == 0
+end
+
+def is_divisible_by_fifteen?(number)
+  number % 15 == 0
+end
+
+
